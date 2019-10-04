4f23e2d846096253d5613775664a21fb82700a1e
Johnny Maravelis <johnny@phoenixafterglow.com>
2019-10-03 22:29:33 -0400

    Completed Homework Instructions take 2 (still a few incomlete sections)

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers_DB.burgers (name) VALUES ("Double-Double Combo");
+INSERT INTO burgers_DB.burgers (name, devoured) VALUES ("Cheeseburger Animal Style", true);
+INSERT INTO burgers_DB.burgers (name, devoured) VALUES ('Double-Double w/out Onion', true);
+INSERT INTO burgers_DB.burgers (name) VALUES ("3x3 Cheeseburger");
+
+SELECT * FROM burgers_DB.burgers;
--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-var orm = require("express");
+// Dependencies
+// =============================================================
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+// const favicon = require('serve-favicon');
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Middleware
+// =============================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public/assets")); 
+// NOTE: ^^^ Now you can do CSS separately Line 17.
+
+// Routing
+// =============================================================
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+const routes = require("./controllers/burgerController.js");
+app.use(routes);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
