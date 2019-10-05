9a2b946654a43bfae84e7623dd862da96a59787a
Johnny Maravelis <johnny@phoenixafterglow.com>
2019-10-05 13:56:17 -0400

    abbreviated without with w/o for seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers_DB.burgers (burger_name) VALUES ("Double-Double Combo");
 INSERT INTO burgers_DB.burgers (burger_name, devoured) VALUES ("Cheeseburger Animal Style", true);
-INSERT INTO burgers_DB.burgers (burger_name, devoured) VALUES ('Double-Double w/out Onion', true);
+INSERT INTO burgers_DB.burgers (burger_name, devoured) VALUES ('Double-Double w/o Onion', true);
 INSERT INTO burgers_DB.burgers (burger_name) VALUES ("3x3 Cheeseburger");
 
 SELECT * FROM burgers_DB.burgers;
